0f16c9a9aa2b8ff12d4bbe51a6baa51b12b20ded
Zhongming <atenliao@gmail.com>
2023-03-15 14:44:07 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  let msg = '';
   // Write your code here!
+  if (feet <= 400){
+    msg = 'This one is on me!';
+  }else if(feet > 400 && feet <= 2000){
+    msg = 'That will be twenty bucks.';
+  }else if(feet > 2000 && feet <= 2500){
+    msg = 'I will gladly take your thirty bucks.';
+  }else{
+    msg = 'No can do.';
+  }
+  return msg;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let togo = city === 'NYC'? 'Ok, sounds good.':'No go.';
+  return togo
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
